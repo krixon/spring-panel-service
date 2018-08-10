9d6c48964f1227165718458773102a4b4fdbcd04
Karl Rixon <karlrixon@gmail.com>
2018-08-10 14:20:00 +0100

    Extract common code to a shared lib

--- src/main/java/com/krixon/panelservice/PanelServiceApplication.java
+++ src/main/java/com/krixon/panelservice/PanelServiceApplication.java
@@ -1,15 +1,21 @@
 package com.krixon.panelservice;
 
+import com.krixon.resourceserver.security.OAuth2UserInfoTokenServices;
+import com.krixon.resourceserver.security.ResourceServerSecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
 @SpringBootApplication
 @EnableResourceServer
+@Configuration
+@Import(ResourceServerSecurityConfig.class)
 public class PanelServiceApplication {
 
     @Autowired
