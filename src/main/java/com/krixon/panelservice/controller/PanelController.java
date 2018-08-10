3c394f1c70285d359ee82fcc528c1d3f2fd46502
Karl Rixon <karlrixon@gmail.com>
2018-08-10 23:45:57 +0100

    Integrate with zipkin for trace visualisation

--- src/main/java/com/krixon/panelservice/controller/PanelController.java
+++ src/main/java/com/krixon/panelservice/controller/PanelController.java
@@ -1,6 +1,9 @@
 package com.krixon.panelservice.controller;
 
+import brave.Span;
+import brave.Tracer;
 import com.krixon.panelservice.dto.Panel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,19 +11,32 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 
 @RestController
-public class PanelController {
+public class PanelController
+{
+    @Autowired
+    private Tracer tracer;
 
     @RequestMapping("/panels")
     @PreAuthorize("#oauth2.hasScope('panel:read')")
-    public ArrayList<Panel> getPanelList() {
-
+    public ArrayList<Panel> getPanelList()
+    {
         // TODO: Real persistence.
 
         ArrayList<Panel> panels = new ArrayList<>();
 
-        panels.add(new Panel("1", "David Lister Appreciation Society"));
-        panels.add(new Panel("2", "Beef and Dairy Network"));
-        panels.add(new Panel("3", "Some Panel Name"));
+        Span span = tracer
+            .nextSpan()
+            .name("fetch panel list")
+            .kind(Span.Kind.SERVER)
+            .start();
+
+        try {
+            panels.add(new Panel("1", "David Lister Appreciation Society"));
+            panels.add(new Panel("2", "Beef and Dairy Network"));
+            panels.add(new Panel("3", "Some Panel Name"));
+        } finally {
+            span.finish();
+        }
 
         return panels;
     }
