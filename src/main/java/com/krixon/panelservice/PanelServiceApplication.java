6974b343b2f18fa269173c087843f44aeb5d1772
Karl Rixon <karlrixon@gmail.com>
2018-08-11 13:55:42 +0100

    Integrate with eureka for service discovery

--- src/main/java/com/krixon/panelservice/PanelServiceApplication.java
+++ src/main/java/com/krixon/panelservice/PanelServiceApplication.java
@@ -6,15 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
+@Configuration
 @SpringBootApplication
 @EnableResourceServer
-@Configuration
+@EnableEurekaClient
 @Import(ResourceServerSecurityConfig.class)
 public class PanelServiceApplication {
 
